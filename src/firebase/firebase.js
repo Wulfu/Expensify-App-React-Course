b3c8ae572e29b3c9a1cfa104cafc0f722dacf5b0
mateusz <mateusz@student.edu>
2018-12-12 00:29:43 +0100

    added webpack and jest config for development and test env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/database';
 import 'firebase/storage';
 
 const config = {
-  apiKey: "AIzaSyCZ9Ngwivc0muGMJLakkXsTSMDRyc7P8h0",
-  authDomain: "react-tutorial-8bc3b.firebaseapp.com",
-  databaseURL: "https://react-tutorial-8bc3b.firebaseio.com",
-  projectId: "react-tutorial-8bc3b",
-  storageBucket: "react-tutorial-8bc3b.appspot.com",
-  messagingSenderId: "45630633985"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
