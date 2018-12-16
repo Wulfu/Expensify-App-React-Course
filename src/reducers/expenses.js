5a5dd4ff0e9eb79798f3107a8d256a176146de5e
mateusz <mateusz@student.edu>
2018-12-16 21:09:07 +0100

    fetching from database, tests

--- src/reducers/expenses.js
+++ src/reducers/expenses.js
@@ -24,6 +24,9 @@ export default (state = expenseReducerDefaultState, action) => {
         }
       });
 
+    case 'SET_EXPENSES':
+      return action.expenses;
+
     default:
       return state;
   }
