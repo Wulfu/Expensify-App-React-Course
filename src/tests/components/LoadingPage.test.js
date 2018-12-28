bd2ac1bdd9e8c4cb7138dbc0f77b7b54dfa807c3
mateusz <mateusz@student.edu>
2018-12-28 14:34:20 +0100

    final working version, minor styles changes can be applied

--- src/tests/components/LoadingPage.test.js
+++ src/tests/components/LoadingPage.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import LoadingPage from '../../components/LoginPage';
+import LoadingPage from '../../components/LoadingPage';
 
 test('should correctly render LoadingPage', () => {
   const wrapper = shallow(<LoadingPage />);
